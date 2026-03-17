3ed806d353aa6e8b4805ca48e5247caf9870ca4b
Tom Gao <zimingg@andrew.cmu.edu>
2026-03-16 21:45:37 -0400

    Added sandbox page for initial ReactFlow demo

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import App from './App.jsx'
+import SandboxPage from './pages/Sandbox.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/sandbox" element={<SandboxPage />} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )
---
+++ src/pages/Sandbox.jsx
@@ -0,0 +1,10 @@
+import '../sandbox/index.css'
+import OverviewFlow from '../sandbox/OverviewFlow.jsx'
+
+export default function SandboxPage() {
+  return (
+    <div style={{ width: '100vw', height: '100vh' }}>
+      <OverviewFlow />
+    </div>
+  )
+}
